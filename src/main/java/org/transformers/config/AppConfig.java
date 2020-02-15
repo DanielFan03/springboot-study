19b32d55b9cc03c9c45ecb8f1db78c04addefa4e
hellboy0621 <hero0621@126.com>
2020-02-15 13:53:23 +0800

    [TicketNo]20200215_135302 [Description]条件装配Bean [Source]NA

--- src/main/java/org/transformers/config/AppConfig.java
+++ src/main/java/org/transformers/config/AppConfig.java
@@ -1,11 +1,15 @@
 package org.transformers.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
+import org.transformers.conditional.DatabaseConditional;
 import org.transformers.entity.User;
 
 import javax.sql.DataSource;
@@ -36,6 +40,29 @@ public class AppConfig {
         props.setProperty("username", "root");
         props.setProperty("password", "tiger");
         DataSource dataSource = null;
+        try {
+            dataSource = BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    // Conditional Bean
+    @Bean(name = "dataSourceConditional", destroyMethod = "close")
+    @Conditional(DatabaseConditional.class)
+    public BasicDataSource getDataSourceConditional(
+            @Value("database.driverName") String driver,
+            @Value("database.url") String url,
+            @Value("database.username") String username,
+            @Value("database.password") String password
+    ) {
+        Properties props = new Properties();
+        props.setProperty("driverClassName", driver);
+        props.setProperty("url", url);
+        props.setProperty("username", username);
+        props.setProperty("password", password);
+        BasicDataSource dataSource = null;
         try {
             dataSource = BasicDataSourceFactory.createDataSource(props);
         } catch (Exception e) {
