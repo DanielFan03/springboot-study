2f47c68de5d9462187fdcbbfbf992e7f103302eb
hellboy0621 <hero0621@126.com>
2020-02-15 13:08:48 +0800

    [TicketNo]20200215_130247 [Description]使用属性文件 [Source]NA

--- src/main/java/org/transformers/SpringbootStudyApplication.java
+++ src/main/java/org/transformers/SpringbootStudyApplication.java
@@ -2,8 +2,10 @@ package org.transformers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:jdbc.properties"}, ignoreResourceNotFound = true)
 public class SpringbootStudyApplication {
 
     public static void main(String[] args) {
